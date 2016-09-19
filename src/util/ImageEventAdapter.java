2bfc80e4d1f0e0c5ab63344542ca0e3785b52cdc
Tomas Colloca Bargallo <tomascllc94@gmail.com>
2016-09-19 15:19:45 -0300

    to maven project + PF stuff

--- src/util/ImageEventAdapter.java
+++
@@ -1,8 +0,0 @@
-package util;
-
-public class ImageEventAdapter implements ImageEventListener {
-  @Override
-  public void onCheckPixelsColor(int x, int y, int width, int height,
-                                 int red, int green, int blue) {
-  }
-}
--- src/util/ImageEventListener.java
+++
@@ -1,6 +0,0 @@
-package util;
-
-public interface ImageEventListener {
-
-  void onCheckPixelsColor(int x, int y, int width, int height, int red, int green, int blue);
-}
